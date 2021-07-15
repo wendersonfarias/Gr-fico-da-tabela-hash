b4dea91780974515c657c6bd8f0d8f6a0cf4a540
Leandro Klein <83349854+Leandro-Klein@users.noreply.github.com>
2021-07-15 07:41:15 -0400

    Add files via upload

---
+++ Dicionario_C/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Hash.h"
+
+int main() {
+    HashStruct hashes;
+    Palavra t_palavra;
+    initHash(&hashes);
+    int opc;
+    while (1)
+    {
+        printf(" \n\n::::::::::::::::::::: MENU :::::::::::::::::::::\n\n\n");
+        printf("  \t[1] - INICIAR TABELA\n\n");
+        printf("  \t[2] - LER ARQUIVO TXT\n\n");
+        printf("  \t[3] - IMPRIMIR LISTA\n\n");
+        printf("  \t[4] - EXIBIR PORCENTAGEM DE OCUPACAO\n\n");
+        printf("  \t[5] - EXIBIR COLISOES\n\n");
+        printf("  \t[6] - MAPA DE COLISOES\n\n");
+        printf("  \t[0] - SAIR\n\n");
+        printf("\n\n::::::Entre com uma opcao >>>> ");
+        scanf ("%d",&opc);
+        if (!opc) break;
+        switch(opc)
+        {
+            case 1:
+                //initHash(&hashes);
+                break;
+            case 2://Abre o arquivo, faz a leitura e grava na tabela
+                carregaArquivo(&hashes,&t_palavra);
+                break;
+            case 3:
+                showHashStruct(&hashes, printPalavra);
+
+                break;
+             case 4: //Calcula a porcentagem de ocupacao da tabela
+                {
+                    porcentagemHash(&hashes);
+                }
+                break;
+             case 5:
+                imprimeColisoes(&hashes, printPalavra);
+                break;
+            case 6:
+                mapaColisoes(&hashes, printPalavra);
+                break;
+            case 0:
+                break;
+            default:
+                printf("\n\n Opcao invalida!!!\n\n");
+                break;
+        }
+    }
+    printf("\n\n ******** Programa finalizado pelo usuario!!! ********\n\n\n");
+    return 0;
+}
